cc7d328e32bdc7d71f41f7ae28bb9b2501c4946a
rishonkumar <rishonkumarrahi@gmail.com>
2022-06-28 11:53:59 +0530

    Input change in React

--- src/App.js
+++ src/App.js
@@ -1,24 +1,29 @@
 import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react';
 
 function App() {
+
+  // we need state to hold current data
+  const[firstName,setFirstName] = useState("")
+
+  function handleChange(event) {
+    // console.log("Changed")
+    // console.log(event)
+    // console.log(event.target)
+    // console.log(event.target.value)
+    setFirstName(event.target.value)
+    
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <form>
+      <input 
+        type="text"  
+        placeholder='First name'
+        onChange={handleChange}
+      />
+    </form>
   );
 }
 
